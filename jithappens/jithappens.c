0c7b7ac7a8abe9fdaa159bd75174b28b6ccb3e2f
Phil Moore <pwmoore57@gmail.com>
2017-06-15 21:51:50 -0700

    More commenting

--- jithappens/jithappens.c
+++ jithappens/jithappens.c
@@ -13,6 +13,8 @@ int main(void)
 
     ks_engine *ks;
     csh cs_handle;
+    
+    // Initialize capstone
     cs_err cserr = cs_open(CS_ARCH_X86, CS_MODE_32, &cs_handle);
 
     if (cserr != CS_ERR_OK) {
@@ -20,6 +22,7 @@ int main(void)
         return cserr;
     }
 
+    // Initialize keystone
     ks_err kserr = ks_open(KS_ARCH_X86, KS_MODE_32, &ks);
     if (kserr != KS_ERR_OK) {
         printf("Could not initialize keystone\n");
@@ -30,16 +33,23 @@ int main(void)
 
     printf("Enter assembly line by line, ending with a ';' on each line. Enter a blank line to execute\n");
     while (1) {
+        // Read a line of input
         char *line = readline("> ");
+
+        // If the line is empty, break
         size_t len = strlen(line);
-        if (!len) {
+        if (!len || line[0] == '\0' || line[0] == ';') {
             break;
         }
+
+        // Make room for the code
         if (!code) {
             code = calloc(1, len + 1);
         } else { 
             code = realloc(code, codelen + len + 1);
         }
+
+        // Copy the code to the end of the line
         memcpy(&code[codelen], line, len);
         codelen += len;
         code[codelen] = '\0';
@@ -48,8 +58,10 @@ int main(void)
 
     if (codelen == 0) {
         printf("You must enter some code!\n");
+        return -1;
     }
 
+    // Now use keystone to assemble the code
     unsigned char *assembled_code = NULL;
     size_t count = 0;
     size_t code_size = 0;
@@ -58,14 +70,18 @@ int main(void)
         return -1;
     }
 
-
+    // Map a page to hold the executable code
     void *executable_code = mmap(NULL, getpagesize(), PROT_WRITE|PROT_READ, MAP_ANON | MAP_PRIVATE, -1, 0);
     if (executable_code == MAP_FAILED) {
         printf("Could not mmap: %s\n", strerror(errno));
         return -1;
     }
+
+    // Copy code into the executable region
     memcpy(executable_code, assembled_code, code_size);
     cs_insn *insn = NULL;
+
+    // Disassemble the code to make sure it's what we expect
     count = cs_disasm(cs_handle, (uint8_t *)executable_code, code_size, (uint64_t)executable_code, 0, &insn);
 
     if (count) {
@@ -74,10 +90,13 @@ int main(void)
         }
     }
 
+    // Set the code as executable
     if (mprotect(executable_code, getpagesize(), PROT_READ | PROT_EXEC)) {
         printf("Could not set code to R-X: %s\n", strerror(errno));
         return -1;
     }
+
+    // Do itc!
     func_t go = (func_t)executable_code;
     long ret = go();
     printf("JIT returned %ld\n", ret);
